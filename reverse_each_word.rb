13f2ec59796922459ece83f940b76740240aa1e5
Kyle Graden <kylegraden3@gmail.com>
2019-03-09 06:10:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(string)
-  new_array = string.split
-  new_array.collect do |word|
+  array = string.split(" ")
+  array.collect do |word|
     word.reverse
   end
-  .join
-end
