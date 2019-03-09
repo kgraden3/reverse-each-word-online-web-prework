dd5fb68171f9f491ecc7cc0838ba857ae73871fa
Kyle Graden <kylegraden3@gmail.com>
2019-03-09 06:31:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = array.collect {|word| puts word.reverse} 
-  new_array.join
+  new_array = array.collect {|word| puts word.reverse}
+  puts new_array.join
 end
